c4ba4654d794e8cc0322b5062dc1237fe4d133d9
Allan1234 <allangoncalves004@gmail.com>
2025-05-24 19:29:18 -0300

    Create player_model.dart

---
+++ lib/player_model.dart
@@ -0,0 +1,11 @@
+class Player {
+  final String name;
+  final int points;
+  final String position;
+
+  Player({
+    required this.name,
+    required this.points,
+    required this.position,
+  });
+}
