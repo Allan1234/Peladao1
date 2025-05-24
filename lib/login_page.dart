9668ac0dbf9aa9c5aba372e97b1e18e9807189dc
Allan1234 <allangoncalves004@gmail.com>
2025-05-24 19:52:23 -0300

    Create login_page.dart

---
+++ lib/login_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Peladão'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextField(
+              controller: usernameController,
+              decoration: InputDecoration(labelText: 'Usuário'),
+            ),
+            TextField(
+              controller: passwordController,
+              decoration: InputDecoration(labelText: 'Senha'),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                print('Usuário: ${usernameController.text}');
+              },
+              child: Text('Entrar'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
