c97ccb264fd29972fffc980872dfda4cb7936281
Allan1234 <allangoncalves004@gmail.com>
2025-05-24 19:44:15 -0300

    Create ranking_page.dart

---
+++ lib/ranking_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RankingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ranking Peladão'),
+      ),
+      body: Center(
+        child: Text('Ranking atualizado em breve!'),
+      ),
+    );
+  }
+}
